7396bf4794d5e267518091976930b5ca8f5d5bbf
Jonathon Coughlin <coughlin.jon@gmail.com>
2016-11-30 10:18:29 -0600

    Added to Jon Scene, Nate Scene Added some Blendo Cuts About to add First Person Drifter controller

---
+++ Assets/Scripts/BlendoCut.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class BlendoCut : MonoBehaviour
+{
+
+    //Target
+    public BlendoSpawn m_SpawnTarget;
+
+    //Components
+    private Collider m_collider;
+
+    // Use this for initialization
+    void Start()
+    {
+        SetComponents();
+    }
+
+    private void SetComponents()
+    {
+        m_collider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.position = m_SpawnTarget.transform.position;
+            other.gameObject.transform.rotation = m_SpawnTarget.transform.rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/SimplePlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class SimplePlayerController : MonoBehaviour
+{
+
+    public float m_Speed;
+
+    //Components
+    private Rigidbody m_rigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        SetComponents();
+    }
+
+    private void SetComponents()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+        float xVel = 0f;
+        float zVel = 0f;
+        
+        if (Input.GetKey(KeyCode.W)) { xVel += m_Speed; }
+        if (Input.GetKey(KeyCode.A)) { zVel += m_Speed; }
+        if (Input.GetKey(KeyCode.S)) { xVel -= m_Speed; }
+        if (Input.GetKey(KeyCode.D)) { zVel -= m_Speed; }
+                      
+        Vector3 localVel = new Vector3(xVel, 0f, zVel);
+        m_rigidbody.velocity = transform.TransformVector(localVel);
+    }
+}
