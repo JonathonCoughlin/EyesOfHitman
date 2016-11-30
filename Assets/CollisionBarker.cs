93e2e210243daea3ac7a511cfaeda42db9a2960e
Jonathon Coughlin <coughlin.jon@gmail.com>
2016-11-30 17:49:44 -0600

    Added Barks to red carpet walkers Modified scenes

---
+++ Assets/CollisionBarker.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Collider))]
+public class CollisionBarker : MonoBehaviour
+{
+
+    //States
+    public bool m_speaking { get; private set; }
+
+    //Barks
+    public List<AudioClip> m_barks = new List<AudioClip>();
+
+    //Components
+    private AudioSource m_Voice;
+    private Collider m_Collider;
+
+    // Use this for initialization
+    void Start()
+    {
+        SetComponents();
+    }
+
+    private void SetComponents()
+    {
+        m_Voice = GetComponent<AudioSource>();
+        m_Collider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") { Bark(); }
+    }
+
+    private void Bark()
+    {
+        int barkID = Random.Range(0, m_barks.Count);
+        m_Voice.PlayOneShot(m_barks[barkID]);
+        Debug.Log("Bark ID: " + barkID);
+    }
+}
---
+++ Assets/Scripts/TableClown.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Animator))]
+public class TableClown : MonoBehaviour {
+
+    //Public animation commands
+    public int animationIDCommand;
+    public bool speakingCommand;
+    public bool drinkingCommand;
+
+    //Private animation states
+    private int m_animationID;
+    private bool m_speaking;
+    private bool m_drinking;
+
+    //Components
+    private Animator m_Animator;
+
+	// Use this for initialization
+	void Start () {
+        SetComponents();
+	}
+
+    private void SetComponents()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        bool animationIDChange = (m_animationID == animationIDCommand);
+        bool speakingChange = (m_speaking == speakingCommand);
+        bool drinkingChange = (m_drinking == drinkingCommand);
+        bool animationChange = animationIDChange || speakingChange || drinkingChange;
+
+	    if (animationChange) { UpdateAnimation(); }
+	}
+
+    private void UpdateAnimation()
+    {
+        //Set States
+        m_animationID = animationIDCommand;
+        m_speaking = speakingCommand;
+        m_drinking = drinkingCommand;
+
+        m_Animator.SetInteger("AnimationID", m_animationID);
+        m_Animator.SetBool("Speaking", m_speaking);
+        m_Animator.SetBool("WithDrink", m_drinking);
+    }
+
+}
