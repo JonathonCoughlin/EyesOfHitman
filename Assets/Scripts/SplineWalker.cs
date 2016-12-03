10259787a44504c4bb2adba1b2f8a72a211eb9e8
Jonathon Coughlin <coughlin.jon@gmail.com>
2016-12-03 09:02:32 -0600

    IntroWork

--- Assets/Scripts/SplineWalker.cs
+++ Assets/Scripts/SplineWalker.cs
@@ -3,15 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 
 
+public enum SplineSpeedType { PercentPerSecond, TimeInterval };
+
 public class CameraSplinePoint
 {
-    public float m_percentPerSecond;
+    public float m_speedPerSegment;
     public bool m_newRotationTarget;
     public GameObject m_rotationTarget;
 
-    public CameraSplinePoint(float pct, bool newTgt, GameObject tgt)
+    public CameraSplinePoint(float spd, bool newTgt, GameObject tgt)
     {
-        m_percentPerSecond = pct;
+        m_speedPerSegment = spd;
         m_newRotationTarget = newTgt;
         m_rotationTarget = tgt;
     }
@@ -24,6 +26,7 @@ public class SplineWalker : MonoBehaviour {
 
     //Walking states
     private bool m_walking;
+    private bool m_paused = false;
     private float m_splinePos;
     public bool m_autoWalk = false;
     public bool m_autoReset = false;
@@ -35,42 +38,64 @@ public class SplineWalker : MonoBehaviour {
     //Custom Speeds
     public bool m_variableSpeed;
     public List<CameraSplinePoint> m_cameraSplinePoints = new List<CameraSplinePoint>();
-    public List<float> percentagePerSecond = new List<float>();
-    public List<bool> setTarget = new List<bool>();
+    public SplineSpeedType m_speedType;
+    public List<float> segmentSpeedValue = new List<float>();
+    public List<bool> useLookTarget = new List<bool>();
     public List<GameObject> currentTarget = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
         ResetSpline();
 
-        for (int ii = 0; ii < percentagePerSecond.Count; ii++ )
+        for (int ii = 0; ii < segmentSpeedValue.Count; ii++ )
         {
-            m_cameraSplinePoints.Add(new CameraSplinePoint(percentagePerSecond[ii], setTarget[ii], currentTarget[ii]));
+            m_cameraSplinePoints.Add(new CameraSplinePoint(segmentSpeedValue[ii], useLookTarget[ii], currentTarget[ii]));
         }
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        
         if (!m_walking)
         {
             if (Input.GetKey(KeyCode.Space) || m_autoWalk) { StartWalking(); }
+        } else if (!m_paused)
+        {
+            ManageWalk();
         } else
         {
-            if (m_variableSpeed)
-            {
-                WalkSplineAtVariableSpeed(Time.deltaTime);
-            }
-            else
-            {
-                WalkSpline(Time.deltaTime);
-            }
+            ManagePause();
         }
+        ManageReset();
 	}
+
+    private void ManageWalk()
+    {
+        if (m_variableSpeed)
+        {
+            WalkSplineAtVariableSpeed(Time.fixedDeltaTime);
+        }
+        else
+        {
+            WalkSpline(Time.fixedDeltaTime);
+        }
+    }
+
+    private void ManagePause()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { PauseWalking(); }
+    }
+
+    private void ManageReset()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) { ResetSpline(); }
+    }
 
     private void ResetSpline()
     {
         m_walking = false;
+        m_paused = false;
         m_splinePos = 0f;
     }
 
@@ -79,10 +104,31 @@ public class SplineWalker : MonoBehaviour {
         m_walking = true;
     }
 
+    public void PauseWalking()
+    {
+        m_paused = !m_paused;
+    }
+
     private void WalkSplineAtVariableSpeed(float deltaTime)
     {
         int currentCurveIndex = Mathf.Min(m_Spline.CurveIDatPercentage(m_splinePos),m_cameraSplinePoints.Count-1);
-        float currentSpeed = m_cameraSplinePoints[currentCurveIndex].m_percentPerSecond;
+        float currentSpeed = 0f;
+        switch (m_speedType)
+        {
+            case SplineSpeedType.PercentPerSecond:
+                {
+                    currentSpeed = m_cameraSplinePoints[currentCurveIndex].m_speedPerSegment;
+                    break;
+                }
+            case SplineSpeedType.TimeInterval:
+                {
+                    float segmentPercentage = 100f / (m_Spline.CurveCount);
+                    currentSpeed = segmentPercentage / m_cameraSplinePoints[currentCurveIndex].m_speedPerSegment;
+                    Debug.Log("speed: " + currentSpeed + "pct/s");
+                    break;
+                }
+        }
+        
 
         if (m_splinePos < 1.0f)
         {
@@ -92,11 +138,11 @@ public class SplineWalker : MonoBehaviour {
             if (m_cameraSplinePoints[currentCurveIndex].m_newRotationTarget)
             {
                 transform.LookAt(m_cameraSplinePoints[currentCurveIndex].m_rotationTarget.transform);
-                Debug.Log("CurveIndex: " + currentCurveIndex);
-                Debug.Log("Spline Pct: " + m_splinePos);
-                Debug.Log("PctPerSec: " + m_cameraSplinePoints[currentCurveIndex].m_percentPerSecond);
-                Debug.Log("New Tgt: " + m_cameraSplinePoints[currentCurveIndex].m_newRotationTarget);
-                Debug.Log("TargetName:" + m_cameraSplinePoints[currentCurveIndex].m_rotationTarget.name);
+                //Debug.Log("CurveIndex: " + currentCurveIndex);
+                //Debug.Log("Spline Pct: " + m_splinePos);
+                //Debug.Log("PctPerSec: " + m_cameraSplinePoints[currentCurveIndex].m_speedPerSegment);
+                //Debug.Log("New Tgt: " + m_cameraSplinePoints[currentCurveIndex].m_newRotationTarget);
+                //Debug.Log("TargetName:" + m_cameraSplinePoints[currentCurveIndex].m_rotationTarget.name);
             }
         }
         else
