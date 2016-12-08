37f935c0e1d084a61a9670072cbb52dc64061c48
Jonathon Coughlin <coughlin.jon@gmail.com>
2016-12-08 16:02:49 -0600

    Beginning Ep2

---
+++ Assets/ConversationTable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConversationTable : MonoBehaviour {
+
+    public 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SpeakingClown.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class SpeakingClown : MonoBehaviour {
+
+    public AudioClip m_Story;
+
+    //Components
+    private AudioSource m_Voice;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
