e0ff4c0b951421770f987bdd9672132d3475f5a1
Jonathon Coughlin <coughlin.jon@gmail.com>
2016-12-11 18:47:51 -0600

    Added conversation tables!!!!

--- Assets/ConversationTable.cs
+++ Assets/ConversationTable.cs
@@ -1,17 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ConversationTable : MonoBehaviour {
+[RequireComponent(typeof(Collider))]
+public class ConversationTable : MonoBehaviour
+{
 
-    public 
+    //Conversation Specifics
+    public ClownAnimations m_speakerAnimation;
+    public AudioClip m_speakerMonologue;
+    public float m_secondsBetweenMonologues;
+    
+    //Conversation States
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    //TableMembers
+    public SpeakingClown m_Speaker;
+
+    //Components
+    private Collider m_collider;
+
+    // Use this for initialization
+    void Start()
+    {
+        SetComponents();
+        SetSpeakingClown();
+    }
+
+    private void SetComponents()
+    {
+        m_collider = GetComponent<Collider>();
+    }
+
+    private void SetSpeakingClown()
+    {
+        m_Speaker.SilenceMe();
+
+        m_Speaker.m_Story = m_speakerMonologue;
+        m_Speaker.m_AnimationStateOverride = m_speakerAnimation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Conversation Management
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_Speaker.BeginSpeaking();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_Speaker.EndSpeaking();
+        }
+    }
 }
