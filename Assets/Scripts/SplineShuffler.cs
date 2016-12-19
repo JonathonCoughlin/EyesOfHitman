e2a117612f5a04f4e2e1e4ae7269436f95a124ac
Jonathon Coughlin <coughlin.jon@gmail.com>
2016-12-18 18:28:27 -0600

    Additional spline updates. Initial Episode 2 room population

--- Assets/Scripts/SplineShuffler.cs
+++ Assets/Scripts/SplineShuffler.cs
@@ -3,10 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 
 
+public class SimpleSplineParameters
+{
+    public WalkerSpeedType m_speedType;
+    public float m_walkSpeed;
+    public WalkerRotationType m_rotationType;
+    public GameObject m_lookTarget;
+    public float m_offsetAngle;
+    public bool m_autoWalk;
+    public bool m_autoReset;
+    public bool m_destroyAtEnd;
+}
+
 [RequireComponent(typeof(SplineWalker))]
 public class SplineShuffler : MonoBehaviour {
 
     private SplineWalker m_Walker;
+
+    public WalkerSpeedType m_speedType;
+    public float m_walkSpeed;
+    public WalkerRotationType m_RotationType;
+    public float m_offsetAngle;
+    public GameObject m_LookAtTarget;
+    public bool m_autoWalk;
+    public bool m_autoReset;
+    public bool m_autoKill;
 
     public List<BezierSpline> m_Splines = new List<BezierSpline>();
 
@@ -23,8 +44,19 @@ public class SplineShuffler : MonoBehaviour {
 
     private void ShuffleSpline()
     {
+        SimpleSplineParameters parameters = new SimpleSplineParameters();
+        parameters.m_speedType = m_speedType;
+        parameters.m_walkSpeed = m_walkSpeed;
+        parameters.m_rotationType = m_RotationType;
+        parameters.m_offsetAngle = m_offsetAngle;
+        parameters.m_lookTarget = m_LookAtTarget;
+        parameters.m_autoWalk = m_autoWalk;
+        parameters.m_autoReset = m_autoReset;
+        parameters.m_destroyAtEnd = m_autoKill;
+
         int splineID = Random.Range(0, m_Splines.Count);
-        m_Walker.m_Spline = m_Splines[splineID];
+        m_Walker.SetSpline(m_Splines[splineID], parameters);
+        
     }
 
 	// Update is called once per frame
