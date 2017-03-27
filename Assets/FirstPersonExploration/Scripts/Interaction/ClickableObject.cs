06d38c5a58895857d6179839b4b874cceb0cf58b
Jonathon Coughlin <coughlin.jon@gmail.com>
2017-03-27 15:01:16 -0500

    subtree test commit

---
+++ Assets/FirstPersonExploration/Scripts/Interaction/ClickableObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FPE_Interaction
+{
+
+    [RequireComponent(typeof(Collider))]
+
+    public class ClickableObject : MonoBehaviour
+    {
+        //Registers clicks by the user, calls logic
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
